7074852be83ba16fe6b4b4486760bd3483985a83
Jeremy Karlsson <jeremy.karlsson@tidal.com>
2021-03-30 00:11:51 +0200

    covert to normal deno

--- index.ts
+++ index.ts
@@ -1,25 +1,31 @@
+import { serve } from "https://deno.land/std@0.91.0/http/server.ts";
 import { resize } from "https://deno.land/x/deno_image/mod.ts";
 import { Sha1 } from "https://deno.land/std@0.91.0/hash/sha1.ts";
 import { compress as brotliEncode } from 'https://deno.land/x/brotli@v0.1.4/mod.ts';
 import { gzipEncode } from 'https://raw.githubusercontent.com/manyuanrong/wasm_gzip/v1.0.0/mod.ts';
+import type { ServerRequest } from 'https://deno.land/std@0.91.0/http/server.ts';
 
-function errorResponse (event: any, msg: string) {
-  event.respondWith(
-    new Response('No url', {
-      status: 500
-    })
-  );
+function reqToURL (req: ServerRequest) {
+  const base = req.conn.localAddr.transport === 'tcp' ? req.conn.localAddr.hostname : 'localhost';
+
+  return new URL(req.url, 'http://' + base);
 }
 
-addEventListener('fetch', async event => {
-  const { searchParams } = new URL(event.request.url);
+function errorResponse (msg: string) {
+  return {
+    body: msg,
+    status: 500
+  };
+}
+
+async function handleRequest (request: ServerRequest) {
+  const { searchParams } = reqToURL(request);
 
   const size = searchParams.get('size');
   const url = searchParams.get('url');
 
   if (!url) {
-    errorResponse(event, 'No url');
-    return;
+    return errorResponse('No url');
   }
 
   const response = await fetch(url);
@@ -32,8 +38,7 @@ addEventListener('fetch', async event => {
   try {
     body = await resize(new Uint8Array(arrayBuffer), { width: size ? parseInt(size, 10) : 500, height: size ? parseInt(size, 10) : 500 });
   } catch (e) {
-    errorResponse(event, e.message);
-    return;
+    return errorResponse(e.message);
   }
 
   const checksum = new Sha1().update(body).hex();
@@ -42,20 +47,28 @@ addEventListener('fetch', async event => {
   headers.set('content-type', response.headers.get('content-type') ?? 'image/jpeg');
   headers.set('Cache-Control', 'public, max-age=3600');
 
-  if (event.request.headers.get('accept-encoding')?.includes('br')) {
+  if (request.headers.get('accept-encoding')?.includes('br')) {
     headers.set('Content-Encoding', 'br');
     body = brotliEncode(body);
-  } else if (event.request.headers.get('accept-encoding')?.includes('gzip')) {
+  } else if (request.headers.get('accept-encoding')?.includes('gzip')) {
     headers.set('Content-Encoding', 'gzip');
     body = gzipEncode(body);
   }
 
   headers.set('Content-Length', String(body.byteLength));
 
-  event.respondWith(
-    new Response(body, {
-      status: 200,
-      headers
-    })
-  );
-});
+  return {
+    body,
+    status: 200,
+    headers
+  };
+}
+
+const server = serve({ port: 8080 });
+console.log(`HTTP web server running.  Access it at:  http://localhost:8080/`);
+
+for await (const request of server) {
+  const response = await handleRequest(request);
+
+  request.respond(response);
+}
